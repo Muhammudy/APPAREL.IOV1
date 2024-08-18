1a304bf2096bc998a40687314e3827e0923e6d9e
Muhammudy <64837648+Muhammudy@users.noreply.github.com>
2024-08-18 18:12:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 
 app.post('/get-sneaker-data', (req, res) => {
     const { category, info } = req.body;

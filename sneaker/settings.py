6670efd5d3dff4d4bdec05761372f5cb754dea60
Thedarkplays <64837648+Thedarkplays@users.noreply.github.com>
2024-08-18 01:52:38 -0500

    Update settings.py

--- sneaker/settings.py
+++ sneaker/settings.py
@@ -122,7 +122,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'

1dd2f18dbfeb5aebc0014be3368c9730062b90cf
Muhammudy <64837648+Muhammudy@users.noreply.github.com>
2024-08-18 14:49:08 -0500

    Update settings.py

--- sneaker/settings.py
+++ sneaker/settings.py
@@ -28,7 +28,7 @@ print("DJANGO_SECRET_KEY:", env('DJANGO_SECRET_KEY'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sneakertracker.onrender.com', 'localhost', '127.0.0.1']
 
